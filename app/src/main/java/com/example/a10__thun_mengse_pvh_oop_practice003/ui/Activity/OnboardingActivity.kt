3dd3f3a2242fa20663940acd0ae1d7837661adbb
M3NGSZE <mengse.thun13@gmail.com>
2025-08-30 01:23:23 +0700

    add background onboarding screen and iocn app

--- app/src/main/java/com/example/a10__thun_mengse_pvh_oop_practice003/ui/Activity/OnboardingActivity.kt
+++ app/src/main/java/com/example/a10__thun_mengse_pvh_oop_practice003/ui/Activity/OnboardingActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.example.a10__thun_mengse_pvh_oop_practice003.R
 
 
 //@SuppressLint("CustomSplashScreen")
@@ -25,8 +31,32 @@ class OnboardingActivity : ComponentActivity() {
 
 @Composable
 fun Onboarding(){
-    Scaffold {
-            paddingValues ->
-        Text("hello onboard", modifier = Modifier.padding(paddingValues))
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Red)
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.g_ngoy),
+            contentDescription = "background onboarding",
+            modifier = Modifier
+                .fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+
     }
+
+//    Column (
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .background(Color.Red)
+//    ) {
+//        Image(
+//            painter = painterResource(id = R.drawable.sentry),
+//            contentDescription = "background onboarding",
+//            modifier = Modifier
+//                .fillMaxSize(),
+//            contentScale = ContentScale.Crop
+//        )
+//    }
 }

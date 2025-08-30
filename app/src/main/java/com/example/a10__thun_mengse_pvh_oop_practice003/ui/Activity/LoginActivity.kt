01471cc6e2a6b4bc5856def02814a8d913585202
M3NGSZE <mengse.thun13@gmail.com>
2025-08-30 18:12:22 +0700

    text field customize shyte

--- app/src/main/java/com/example/a10__thun_mengse_pvh_oop_practice003/ui/Activity/LoginActivity.kt
+++ app/src/main/java/com/example/a10__thun_mengse_pvh_oop_practice003/ui/Activity/LoginActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -14,8 +15,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,8 +26,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -79,9 +87,32 @@ private fun Login(){
         var text by remember { mutableStateOf("") }
 
         TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(80.dp),
             value = text,
             onValueChange = { newText -> text = newText },
-            label = { Text("Email")},
+            label = {
+                Text(
+                    text = "Email",
+                    fontSize = 18.sp,
+                    color = Color.Black,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+            },
+            colors = TextFieldDefaults.colors(
+                unfocusedContainerColor = Color.Transparent,
+                focusedContainerColor = Color.Transparent,
+//                focusedIndicatorColor = Color(0xFF919090),
+                unfocusedIndicatorColor = Color(0x73919090)
+            ),
+            singleLine = true,
+            textStyle = TextStyle(
+                fontSize = 18.sp
+            )
         )
+
+
+
     }
 }

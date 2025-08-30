6d0c778a66eaad274efe32855112f201c4547096
M3NGSZE <mengse.thun13@gmail.com>
2025-08-30 13:37:11 +0700

    login screen first text field

--- app/src/main/java/com/example/a10__thun_mengse_pvh_oop_practice003/MainActivity.kt
+++ app/src/main/java/com/example/a10__thun_mengse_pvh_oop_practice003/MainActivity.kt
@@ -11,10 +11,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -40,14 +38,6 @@ class MainActivity : ComponentActivity() {
                 finish()
             }
         }
-    }
-}
-
-@Composable
-fun Onboard(){
-    Scaffold {
-        paddingValues ->
-        Text("hello onboard", modifier = Modifier.padding(paddingValues))
     }
 }
 
--- app/src/main/java/com/example/a10__thun_mengse_pvh_oop_practice003/ui/Activity/LoginActivity.kt
+++ app/src/main/java/com/example/a10__thun_mengse_pvh_oop_practice003/ui/Activity/LoginActivity.kt
@@ -4,11 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.a10__thun_mengse_pvh_oop_practice003.R
 
 class LoginActivity : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,8 +42,46 @@ class LoginActivity : ComponentActivity(){
 
 @Composable
 private fun Login(){
-    Scaffold {
-        paddingValues ->
-        Text("Login screen", modifier = Modifier.padding(paddingValues))
+    Column (
+        modifier = Modifier
+            .systemBarsPadding()
+            .padding(16.dp)
+    ){
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.2f),
+            contentAlignment = Alignment.Center
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.orange_carrot),
+                contentDescription = "Orange Carrot",
+                modifier = Modifier.size(60.dp)
+            )
+        }
+
+        Text(
+            text = "Loging",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Medium
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Text(
+            text = "Enter your emails and password",
+            fontSize = 17.sp,
+            color = Color(0xFF919090),
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        var text by remember { mutableStateOf("") }
+
+        TextField(
+            value = text,
+            onValueChange = { newText -> text = newText },
+            label = { Text("Email")},
+        )
     }
 }

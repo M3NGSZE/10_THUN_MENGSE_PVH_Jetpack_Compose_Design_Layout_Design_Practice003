4281360b4fe0ceb439b9d7046e606961a7911885
M3NGSZE <mengse.thun13@gmail.com>
2025-08-30 12:26:35 +0700

    finish onboarding screen

---
+++ app/src/main/java/com/example/a10__thun_mengse_pvh_oop_practice003/ui/Activity/LoginActivity.kt
@@ -0,0 +1,29 @@
+package com.example.a10__thun_mengse_pvh_oop_practice003.ui.Activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+class LoginActivity : ComponentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Login()
+        }
+    }
+}
+
+@Composable
+private fun Login(){
+    Scaffold {
+        paddingValues ->
+        Text("Login screen", modifier = Modifier.padding(paddingValues))
+    }
+}
--- app/src/main/java/com/example/a10__thun_mengse_pvh_oop_practice003/ui/Activity/OnboardingActivity.kt
+++ app/src/main/java/com/example/a10__thun_mengse_pvh_oop_practice003/ui/Activity/OnboardingActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a10__thun_mengse_pvh_oop_practice003.ui.Activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,16 +10,30 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.a10__thun_mengse_pvh_oop_practice003.MainActivity
 import com.example.a10__thun_mengse_pvh_oop_practice003.R
 
-
-//@SuppressLint("CustomSplashScreen")
 class OnboardingActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,11 +45,12 @@ class OnboardingActivity : ComponentActivity() {
 }
 
 @Composable
-fun Onboarding(){
+private fun Onboarding(){
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Red)
+            .background(Color.Red),
+        contentAlignment = Alignment.BottomEnd
     ){
         Image(
             painter = painterResource(id = R.drawable.g_ngoy),
@@ -44,19 +60,68 @@ fun Onboarding(){
             contentScale = ContentScale.Crop
         )
 
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.5f)
+                .background(Color(0x3B2E2E2E)),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.carrot_logo),
+                contentDescription = "App Logo",
+                modifier = Modifier.size(60.dp)
+            )
+
+            Spacer(
+                modifier = Modifier.height(20.dp)
+            )
+
+            Text(
+                text = "  Welcome\nto our store",
+                fontSize = 50.sp,
+                color = Color.White,
+                fontWeight = FontWeight.SemiBold
+            )
+
+            Spacer(
+                modifier = Modifier.height(10.dp)
+            )
+
+            Text(
+                text = "Get your groceries in as fast as one hour",
+                fontSize = 17.sp,
+                color = Color(0xFEB5B1B1),
+            )
+
+            Spacer(
+                modifier = Modifier.height(40.dp)
+            )
+
+            val context = LocalContext.current
+
+            Button(
+                onClick = {
+                    val intent = Intent(context, LoginActivity::class.java)
+                    context.startActivity(intent)
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF53B175)
+                ),
+                shape = RoundedCornerShape(20.dp),
+                modifier = Modifier
+                    .width(370.dp)
+                    .height(70.dp)
+            ) {
+                Text(
+                    text = "Get Started",
+                    fontSize = 20.sp
+                )
+            }
+        }
+
     }
 
-//    Column (
-//        modifier = Modifier
-//            .fillMaxSize()
-//            .background(Color.Red)
-//    ) {
-//        Image(
-//            painter = painterResource(id = R.drawable.sentry),
-//            contentDescription = "background onboarding",
-//            modifier = Modifier
-//                .fillMaxSize(),
-//            contentScale = ContentScale.Crop
-//        )
-//    }
 }

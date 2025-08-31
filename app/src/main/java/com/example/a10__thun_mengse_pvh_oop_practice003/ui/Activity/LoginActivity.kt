568b658d3a1c71f386f936740b72892901eeee87
M3NGSZE <mengse.thun13@gmail.com>
2025-08-31 09:48:29 +0700

    organize login activity

--- app/src/main/java/com/example/a10__thun_mengse_pvh_oop_practice003/ui/Activity/LoginActivity.kt
+++ app/src/main/java/com/example/a10__thun_mengse_pvh_oop_practice003/ui/Activity/LoginActivity.kt
@@ -6,49 +6,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.a10__thun_mengse_pvh_oop_practice003.R
-import com.example.a10__thun_mengse_pvh_oop_practice003.ui.component.NectarButton
+import com.example.a10__thun_mengse_pvh_oop_practice003.ui.component.NectarPassField
 import com.example.a10__thun_mengse_pvh_oop_practice003.ui.component.NectarTextField
+import com.example.a10__thun_mengse_pvh_oop_practice003.ui.component.TopSection
 
 class LoginActivity : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -67,223 +51,146 @@ private fun Login(){
             .systemBarsPadding()
             .padding(25.dp)
     ){
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.2f),
-            contentAlignment = Alignment.TopCenter
-        ){
-            Image(
-                painter = painterResource(id = R.drawable.orange_carrot),
-                contentDescription = "Orange Carrot",
-                modifier = Modifier
-                    .size(80.dp)
-                    .padding(top = 20.dp)
-            )
-        }
 
-        Text(
-            text = "Loging",
-            fontSize = 30.sp,
-            fontWeight = FontWeight.Medium
-        )
+        val title = "Loging"
+        val description = "Enter your emails and password"
 
-        Spacer(modifier = Modifier.height(15.dp))
-
-        Text(
-            text = "Enter your emails and password",
-            fontSize = 17.sp,
-            fontWeight = FontWeight.W500,
-            color = Color(0xFF919090),
-        )
+        TopSection(title, description)
 
         Spacer(modifier = Modifier.height(35.dp))
+
+        // form section
 
         var email by remember { mutableStateOf("") }
 
         NectarTextField("Email") { e -> email = e}
 
-//        TextField(
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .height(80.dp),
-//            value = email,
-//            onValueChange = { newText -> email = newText },
-//            label = {
-//                Text(
-//                    text = "Email",
-//                    fontSize = 20.sp,
-//                    fontWeight = FontWeight.Medium,
-//                    color = Color(0xFF919090),
-//                    modifier = Modifier.padding(bottom = 8.dp)
-//                )
-//            },
-//            colors = TextFieldDefaults.colors(
-//                unfocusedContainerColor = Color.Transparent,
-//                focusedContainerColor = Color.Transparent,
-//                focusedIndicatorColor = Color(0xFF53B175),
-//                unfocusedIndicatorColor = Color(0x73919090)
-//            ),
-//            singleLine = true,
-//            textStyle = TextStyle(
-//                fontSize = 18.sp
-//            )
-//        )
-
         Spacer(modifier = Modifier.height(35.dp))
 
         var password by remember { mutableStateOf("") }
-        var passwordVisibility by remember { mutableStateOf(false) }
 
-        val icon = if (passwordVisibility)
-            painterResource(id = R.drawable.visible_icon)
-        else
-            painterResource(id = R.drawable.invisible_icon)
-
-
-        TextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(80.dp),
-            value = password,
-            onValueChange = { password = it },
-            label = {
-                Text(
-                    text = "Password",
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Medium,
-                    color = Color(0xFF919090),
-                    modifier = Modifier.padding(bottom = 8.dp)
-                )
-            },
-            colors = TextFieldDefaults.colors(
-                unfocusedContainerColor = Color.Transparent,
-                focusedContainerColor = Color.Transparent,
-                focusedIndicatorColor = Color(0xFF53B175),
-                unfocusedIndicatorColor = Color(0x73919090)
-            ),
-            singleLine = true,
-            textStyle = TextStyle(
-                fontSize = 18.sp
-            ),
-            trailingIcon = {
-                IconButton(
-                    onClick = {
-                        passwordVisibility = !passwordVisibility
-                    }
-                ) {
-                    Icon(
-                        painter = icon,
-                        contentDescription = "Visible Icon",
-                        modifier = Modifier.size(20.dp),
-                    )
-                }
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Password, // Keyboard type
-                imeAction = ImeAction.Done            // Action button on keyboard
-            ),
-            visualTransformation = if (passwordVisibility)
-                VisualTransformation.None
-            else
-                PasswordVisualTransformation(),
-        )
+        NectarPassField("Password1") { p -> password = p}
 
         var validationState by remember { mutableStateOf(true) }
 
+        // provide context of current screen use for navigation
         val context = LocalContext.current
-        val text = "Log in"
 
-        val validation : (String, String, Context) -> Unit = {
-                email, password , context->
-            if (email == "chanelle@gmail.com" && password == "Chanelle123@"){
-                val intent = Intent(context, HomeActivity::class.java)
-                context.startActivity(intent)
-                validationState = true
-            } else {
-                validationState = false
-            }
-        }
+        Alert(validationState)
 
-        if (!validationState){
-
-            Spacer(modifier = Modifier.height(20.dp))
-
-            Text(
-                text = "Invalid Email or Password",
-                fontSize = 17.sp,
-                color = Color.Red
-            )
-        }
-
-        Row (
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 20.dp),
-            horizontalArrangement = Arrangement.End
-        ){
-            Text(
-                text = "Forgot Password?",
-                fontSize = 16.sp,
-                modifier = Modifier
-                    .clickable{
-
-                    }
-            )
-        }
+        ForgotPassword()
 
         Spacer(modifier = Modifier.height(30.dp))
 
-
-
-//        Spacer(modifier = Modifier.height(10.dp))
-
-//        NectarButton(context, HomeActivity(), text)
-        Button(
-            onClick = {
-//                validationState = validation(email, password)
-                validation(email, password, context)
-            },
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xFF53B175)
-            ),
-            shape = RoundedCornerShape(20.dp),
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(80.dp)
-        ) {
-            Text(
-                text = text,
-                fontSize = 23.sp,
-                fontWeight = FontWeight.ExtraBold
-            )
-        }
+        LoginButton(email, password, context) { v -> validationState = v }
 
         Spacer(modifier = Modifier.height(30.dp))
 
-        Row (
-            modifier = Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ){
-            Text(
-                text = "Don't have an account? ",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Medium
-            )
-            Text(
-                text = " Signup",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Medium,
-                color = Color(0xFF53B175),
-                modifier = Modifier
-                    .clickable{
-                        val intent = Intent(context, SignUpActivity::class.java)
-                        context.startActivity(intent)
-                    }
-            )
-        }
+        Signup(context)
     }
 }
 
 
+// alert section
+@Composable
+private fun Alert(validationState: Boolean){
+    if (!validationState){
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            text = "Invalid Email or Password",
+            fontSize = 17.sp,
+            color = Color.Red
+        )
+    }
+}
+
+// forgot password section
+@Composable
+private fun ForgotPassword(){
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 20.dp),
+        horizontalArrangement = Arrangement.End
+    ){
+        Text(
+            text = "Forgot Password?",
+            fontSize = 16.sp,
+            modifier = Modifier
+                .clickable{
+
+                }
+        )
+    }
+}
+
+//login section
+@Composable
+private fun LoginButton(email: String, password: String, context: Context, setValidationState:(Boolean)-> Unit){
+
+    var validationState by remember { mutableStateOf(true) }
+    setValidationState(validationState)
+
+    Button(
+        onClick = {
+            validation(email, password, context){
+                    it -> validationState = it
+            }
+        },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFF53B175)
+        ),
+        shape = RoundedCornerShape(20.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp)
+    ) {
+        Text(
+            text = "Log in",
+            fontSize = 23.sp,
+            fontWeight = FontWeight.ExtraBold
+        )
+    }
+}
+
+// validation
+private val validation : (String, String, Context, (Boolean) -> Unit) -> Unit = {
+        email, password , context, validateState->
+    if (email == "chanelle@gmail.com" && password == "Chanelle123@"){
+        val intent = Intent(context, HomeActivity::class.java)
+        context.startActivity(intent)
+//        validateState(true)
+    } else {
+        validateState(false)
+    }
+}
+
+
+// signup section
+@Composable
+private fun Signup(context: Context){
+    Row (
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ){
+        Text(
+            text = "Don't have an account? ",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Medium
+        )
+        Text(
+            text = " Signup",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color(0xFF53B175),
+            modifier = Modifier
+                .clickable{
+                    val intent = Intent(context, SignUpActivity::class.java)
+                    context.startActivity(intent)
+                }
+        )
+    }
+}
--- app/src/main/java/com/example/a10__thun_mengse_pvh_oop_practice003/ui/component/Coponents.kt
+++ app/src/main/java/com/example/a10__thun_mengse_pvh_oop_practice003/ui/component/Coponents.kt
@@ -3,13 +3,21 @@ package com.example.a10__thun_mengse_pvh_oop_practice003.ui.component
 import android.content.Context
 import android.content.Intent
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -18,14 +26,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.a10__thun_mengse_pvh_oop_practice003.R
 import org.w3c.dom.Text
 
 @Composable
@@ -83,5 +98,106 @@ fun NectarTextField(label: String, setEmail: (String) -> Unit){
         textStyle = TextStyle(
             fontSize = 18.sp
         )
+    )
+}
+
+@Composable
+fun NectarPassField(label: String, setPassword: (String) -> Unit){
+
+    var password by remember { mutableStateOf("") }
+    setPassword(password)
+
+    var passwordVisibility by remember { mutableStateOf(false) }
+
+    val icon = if (passwordVisibility)
+        painterResource(id = R.drawable.visible_icon)
+    else
+        painterResource(id = R.drawable.invisible_icon)
+
+
+    TextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp),
+        value = password,
+        onValueChange = { password = it },
+        label = {
+            Text(
+                text = label,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color(0xFF919090),
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+        },
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = Color.Transparent,
+            focusedContainerColor = Color.Transparent,
+            focusedIndicatorColor = Color(0xFF53B175),
+            unfocusedIndicatorColor = Color(0x73919090)
+        ),
+        singleLine = true,
+        textStyle = TextStyle(
+            fontSize = 18.sp
+        ),
+        trailingIcon = {
+            IconButton(
+                onClick = {
+                    passwordVisibility = !passwordVisibility
+                }
+            ) {
+                Icon(
+                    painter = icon,
+                    contentDescription = "Visible Icon",
+                    modifier = Modifier.size(20.dp),
+                )
+            }
+        },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Password, // Keyboard type
+            imeAction = ImeAction.Done            // Action button on keyboard
+        ),
+        visualTransformation = if (passwordVisibility)
+            VisualTransformation.None
+        else
+            PasswordVisualTransformation(),
+    )
+}
+
+@Composable
+fun TopSection(title: String, description: String){
+
+    // icon section
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.2f),
+        contentAlignment = Alignment.TopCenter
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.orange_carrot),
+            contentDescription = "Orange Carrot",
+            modifier = Modifier
+                .size(80.dp)
+                .padding(top = 20.dp)
+        )
+    }
+
+    // text section
+
+    Text(
+        text = title,
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Medium
+    )
+
+    Spacer(modifier = Modifier.height(15.dp))
+
+    Text(
+        text = description,
+        fontSize = 17.sp,
+        fontWeight = FontWeight.W500,
+        color = Color(0xFF919090),
     )
 }

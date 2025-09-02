58c98668491af14c3aeffc3ba750fac3edb98336
M3NGSZE <mengse.thun13@gmail.com>
2025-09-03 01:09:25 +0700

    last time it disappear

--- app/src/main/java/com/example/a10__thun_mengse_pvh_oop_practice003/screen/explore/exploreProduct/ExploreProductScreen.kt
+++ app/src/main/java/com/example/a10__thun_mengse_pvh_oop_practice003/screen/explore/exploreProduct/ExploreProductScreen.kt
@@ -3,15 +3,11 @@ package com.example.a10__thun_mengse_pvh_oop_practice003.screen.explore.exploreP
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddRoad
 import androidx.compose.material.icons.filled.ArrowBackIosNew
-import androidx.compose.material.icons.filled.FilterList
 import androidx.compose.material.icons.filled.Tune
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text

9c40f4237a4f212a9693ee9bbaadf1150868a348
M3NGSZE <mengse.thun13@gmail.com>
2025-09-07 15:15:59 +0700

    shit accidently commit

--- app/src/main/java/com/example/a10__thun_mengse_pvh_oop_practice003/screen/detailProduct/DetailSection.kt
+++ app/src/main/java/com/example/a10__thun_mengse_pvh_oop_practice003/screen/detailProduct/DetailSection.kt
@@ -1,2 +1,78 @@
 package com.example.a10__thun_mengse_pvh_oop_practice003.screen.detailProduct
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.a10__thun_mengse_pvh_oop_practice003.data.ProductMoreFields
+
+@Composable
+fun DetailProductSection(item: ProductMoreFields, navController: NavController){
+
+    var isFavorite by remember { mutableStateOf(false) }
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(
+                text = item.name,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            IconButton(
+                onClick = {
+                    isFavorite = !isFavorite
+                }
+            ) {
+                if (isFavorite){
+                    Icon(
+                        imageVector = Icons.Default.Favorite,
+                        contentDescription = "Favorite",
+                        tint = Color.Red
+                    )
+                } else{
+                    Icon(
+                        imageVector = Icons.Default.FavoriteBorder,
+                        contentDescription = "Favorite",
+                        tint = Color.Gray
+                    )
+                }
+            }
+        }
+
+        Text(
+            text = item.des,
+            color = Color.Gray,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium
+        )
+    }
+}
